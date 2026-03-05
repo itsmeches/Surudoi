9c77559e3db2e3a2b0c1ded044b84047d29250f3
Chester Jefferson Andaya <chester.andaya11@gmail.com>
2026-03-05 04:03:26 +0200

    Changes in contact me

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -28,7 +28,7 @@ export const Footer = () => {
       <div className="container">
         <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8">
           <div className="text-white/40">
-            &copy; 2024, All rights reserved.
+            &copy; 2026, All rights reserved.
           </div>
 
           <nav className="flex flex-col md:flex-row items-center gap-8">
